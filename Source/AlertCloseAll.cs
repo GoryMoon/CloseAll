c5c42ac4ab9e8e3169464aa16fa54d47e241ad7f
GoryMoon <gurreja@gmail.com>
2020-03-10 03:48:57 +0100

    Initial commit

---
+++ Source/AlertCloseAll.cs
@@ -0,0 +1,28 @@
+﻿using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace CloseAll
+{
+    public class AlertCloseAll: Alert
+    {
+        public AlertCloseAll()
+        {
+            defaultLabel = "CloseAll".Translate();
+            defaultPriority = AlertPriority.High;
+            defaultExplanation = "CloseAllDesc".Translate();
+        }
+
+        protected override Color BGColor => new Color(1f, 1f, 1f, 0.2f);
+
+        protected override void OnClick()
+        {
+            Find.LetterStack.LettersListForReading.ListFullCopy().ForEach(letter => Find.LetterStack.RemoveLetter(letter));
+        }
+
+        public override AlertReport GetReport()
+        {
+            return Find.LetterStack.LettersListForReading.Count > 0;
+        }
+    }
+}
