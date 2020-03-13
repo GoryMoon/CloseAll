82390a47701916f830e4e9eb05934eca45240e4a
GoryMoon <gurreja@gmail.com>
2020-03-13 05:09:07 +0100

    Removed "ding" when alert shows

--- Source/AlertCloseAll.cs
+++ Source/AlertCloseAll.cs
@@ -1,4 +1,8 @@
-﻿using RimWorld;
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using HarmonyLib;
+using RimWorld;
 using UnityEngine;
 using Verse;
 
@@ -23,6 +27,41 @@ namespace CloseAll
         public override AlertReport GetReport()
         {
             return Find.LetterStack.LettersListForReading.Count > 0;
+        }
+    }
+
+    
+    [HarmonyPatch(typeof(Alert))]
+    [HarmonyPatch("Notify_Started")]
+    public static class Alert_Notify_Started_Patch
+    {
+        static readonly MethodInfo _levelLoad = AccessTools.Property(typeof(Time), "timeSinceLevelLoad").GetMethod;
+        static readonly MethodInfo _checkType =
+            SymbolExtensions.GetMethodInfo(() => CheckType(null));
+        
+        static bool CheckType(Alert alert)
+        {
+            return alert.GetType() == typeof(AlertCloseAll);
+        }
+        
+        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            var injected = false;
+            var foundLabel = false;
+            var label = new Label?();
+            foreach (var instruction in instructions)
+            {
+                if (!foundLabel && instruction.Branches(out label)) foundLabel = true;
+                if (!injected && instruction.Calls(_levelLoad) && label.HasValue)
+                {
+                    yield return new CodeInstruction(OpCodes.Ldarg_0);
+                    yield return new CodeInstruction(OpCodes.Call, _checkType);
+                    yield return new CodeInstruction(OpCodes.Brtrue, label.Value);
+
+                    injected = true;
+                }
+                yield return instruction;
+            }
         }
     }
 }
