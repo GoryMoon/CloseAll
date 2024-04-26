750941866b3e207be51b8fcf5c6f14ac8976b888
GoryMoon <gurreja@gmail.com>
2024-04-26 23:17:25 +0200

    Updated to Rimworld 1.5

--- Source/AlertCloseAll.cs
+++ Source/AlertCloseAll.cs
@@ -34,18 +34,19 @@ namespace CloseAll
     
     [HarmonyPatch(typeof(Alert))]
     [HarmonyPatch("Notify_Started")]
-    public static class Alert_Notify_Started_Patch
+    public static class AlertNotifyStartedPatch
     {
-        static readonly MethodInfo _levelLoad = AccessTools.Property(typeof(Time), "timeSinceLevelLoad").GetMethod;
-        static readonly MethodInfo _checkType =
+        private static readonly MethodInfo LevelLoad = AccessTools.Property(typeof(Time), "timeSinceLevelLoad").GetMethod;
+
+        private static readonly MethodInfo CheckTypeInfo =
             SymbolExtensions.GetMethodInfo(() => CheckType(null));
-        
-        static bool CheckType(Alert alert)
+
+        private static bool CheckType(Alert alert)
         {
             return alert.GetType() == typeof(AlertCloseAll);
         }
-        
-        static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+
+        private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
         {
             var injected = false;
             var foundLabel = false;
@@ -53,10 +54,10 @@ namespace CloseAll
             foreach (var instruction in instructions)
             {
                 if (!foundLabel && instruction.Branches(out label)) foundLabel = true;
-                if (!injected && instruction.Calls(_levelLoad) && label.HasValue)
+                if (!injected && instruction.Calls(LevelLoad) && label.HasValue)
                 {
                     yield return new CodeInstruction(OpCodes.Ldarg_0);
-                    yield return new CodeInstruction(OpCodes.Call, _checkType);
+                    yield return new CodeInstruction(OpCodes.Call, CheckTypeInfo);
                     yield return new CodeInstruction(OpCodes.Brtrue, label.Value);
 
                     injected = true;
